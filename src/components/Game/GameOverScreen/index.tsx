693b219b8788bec8395edae8d3c0de22b80da505
brigadiertz <rg27@bk.ru>
2018-07-23 16:31:57 +0300

    удар об стену и экран GameOver

---
+++ src/components/Game/GameOverScreen/index.tsx
@@ -0,0 +1,25 @@
+import * as React from 'react'
+
+
+interface Props{
+    score:number,
+}
+
+class GameOverScreen extends React.Component<Props,{}>{
+    render(){
+        return(
+            <div>
+                <h2>Конец игры</h2>
+                <h3>Очки: {this.props.score}</h3>
+                <p>Пробел  - новая игра</p>
+                <button
+                    onClick={()=>{
+                       window.location.reload()
+                    }}
+                >Перезагрузить игру</button>
+            </div>
+        )
+    }
+}
+
+export default GameOverScreen
--- src/components/Game/index.tsx
+++ src/components/Game/index.tsx
@@ -1,9 +1,11 @@
 import * as React from 'react';
 import clone from 'clone'
 import random from 'random';
+import equal from 'deep-equal';
 
 import './style.scss';
 import StartScreen from './StartScreen'
+import GameOverScreen from './GameOverScreen'
 import Table from './Table'
 
 
@@ -13,6 +15,7 @@ interface Props{
 
 interface State{
     isStart:boolean,
+    gameOver:boolean,
     settings:{
         width:number,
         height:number,
@@ -23,31 +26,37 @@ interface State{
         x:number,
         y:number
     },
-    apple?:number[]
+    apple?:number[],
+    score:number
 }
 
 let timerId ;
 
+let defaultSnake=[
+    [0,0],
+    [0,1],
+    [0,2],
+]
+let defaultDirection = {
+    x:1,
+    y:0
+}
+let defaultScore = 0;
+
 class Game extends React.Component<Props,State>{
 
-    state:State={
+    state:State= {
         isStart:false,
+        gameOver:false,
         settings:{
             width:10,
             height:10,
             speed:5
         },
-        snake:[
-            [0,0],
-            [0,1],
-            [0,2],
-            [0,3]
-        ],
-        direction:{
-            x:1,
-            y:0
-        },
-    }
+        snake:defaultSnake,
+        direction:defaultDirection,
+        score:defaultScore
+    };
 
     componentWillMount(){
         document.addEventListener("keydown",(e)=> this.handleKeyDown(e));
@@ -68,8 +77,16 @@ class Game extends React.Component<Props,State>{
             case 37: // стрелка влево
                 this.handleChangeDirectionLeft()
                 break;
+            case 32: // пробел
+                this. handleStartNewGame()
             default:
                 break;
+        }
+    }
+
+    handleStartNewGame(){
+        if(!this.state.isStart){
+            this.handleOnStart(this.state.settings)
         }
     }
 
@@ -100,7 +117,11 @@ class Game extends React.Component<Props,State>{
     handleOnStart(settings){
         this.setState({
             isStart:true,
-            settings:settings
+            gameOver:false,
+            settings:settings,
+            snake:defaultSnake,
+            direction:defaultDirection,
+            score:defaultScore
         },()=>{
            this.funcNewApple()
            this.funcTimerStart()
@@ -109,12 +130,21 @@ class Game extends React.Component<Props,State>{
 
     }
 
+
     funcTimerStart(){
+        clearInterval(timerId);
         timerId = setInterval(()=> {
+            if(!this.state.isStart){
+                clearInterval(timerId);
+            }
             this.funcMove()
         }, (1000/this.state.settings.speed)*1.9);
     }
     funcMove(callback:Function=()=>{}){
+
+        if(this.state.gameOver){
+            return false;
+        }
 
         let oldSnake = clone(this.state.snake);
         let newSnake = clone(this.state.snake);
@@ -134,6 +164,11 @@ class Game extends React.Component<Props,State>{
             newSnake[headIndex][1] === this.state.apple[1]
         ){
             newSnake.splice(-1,0,oldSnake[headIndex]);
+            this.setState({
+                score:++this.state.score
+            })
+
+            this.funcNewApple()
         }
         else{
             //смещаем тело
@@ -142,7 +177,16 @@ class Game extends React.Component<Props,State>{
             }
         }
 
-
+        //удар об стену
+        let head = newSnake[newSnake.length-1];
+        if(
+            head[0] === -1
+            ||  head[1] === -1
+            ||  head[0] > this.state.settings.width-1
+            ||  head[1] > this.state.settings.height-1
+        ){
+          this.funcGameOver();
+        }
 
         this.setState({
             snake:newSnake
@@ -150,11 +194,27 @@ class Game extends React.Component<Props,State>{
             callback()
         })
     }
+    funcGameOver(){
+        clearInterval(timerId);
+        this.setState({
+            gameOver:true,
+            isStart:false
+        })
+    }
     funcChangeDirection(direction){
+
+        let oldDirection = this.state.direction;
+
+        //не допускается противоположное направление
+        if(oldDirection.x === -direction.x || oldDirection.y === - direction.y){
+            return false;
+        }
+
+        clearInterval(timerId);
         this.setState({
             direction:direction
         },()=>{
-            clearInterval(timerId);
+
 
             this.funcMove(()=>{
                 this.funcTimerStart()
@@ -164,10 +224,22 @@ class Game extends React.Component<Props,State>{
         })
     }
     funcNewApple(){
+
         let apple = [
-            random.int(0,this.state.settings.width-1),
-            random.int(0,this.state.settings.height-1)
+             random.int(0,this.state.settings.width-1),
+             random.int(0,this.state.settings.height-1)
         ]
+
+        let isIncludeSnake = false;
+
+        isIncludeSnake = this.state.snake.some((elem)=>{
+            return equal(elem,apple)
+        })
+
+       if(isIncludeSnake){
+           this.funcNewApple()
+          return false;
+       }
 
         this.setState({
             apple
@@ -179,10 +251,15 @@ class Game extends React.Component<Props,State>{
             <div className="Game">
                 {
                     !this.state.isStart ?
-                        <StartScreen
-                            settings={this.state.settings}
-                            onStart={(settings)=>this.handleOnStart(settings)}
-                        />
+                        this.state.gameOver?
+                            <GameOverScreen
+                             score={this.state.score}
+                            />
+                            :
+                            <StartScreen
+                                settings={this.state.settings}
+                                onStart={(settings)=>this.handleOnStart(settings)}
+                            />
                         :
                         <div>
                             <Table
@@ -191,6 +268,9 @@ class Game extends React.Component<Props,State>{
                                 snake={this.state.snake}
                                 apple={this.state.apple}
                             />
+                            <div>
+                                <h2>Очки: {this.state.score}</h2>
+                            </div>
                         </div>
                 }
             </div>
